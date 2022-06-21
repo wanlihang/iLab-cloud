595d1361bb6a3b6fa8224567aff5fb6d60a000cd
wanlihang <lihang_wan@outlook.com>
2022-06-21 15:41:13 +0800

    feat: 整合 spring cloud alibaba sidecar 服务

---
+++ ilab-service/src/main/java/com/ilab/service/ILabServiceApplication.java
@@ -0,0 +1,29 @@
+package com.ilab.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 异构服务
+ *
+ * @author wlh
+ */
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class ILabServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ILabServiceApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  异构服务模块启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+
+}
